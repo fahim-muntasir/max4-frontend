ddb29a71476d6d9f73992c03c465ac78ea625b82
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-05-13 11:45:43 +0600

    ...

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className=" text-3xl text-white ">Hi, welcome to max4</h2>
+      <h2 className=" text-3xl text-amber-400 ">Hi, welcome to max4</h2>
+      <p className="text-lg text-amber-400">Website will be updated very soon!</p>
+      <p className="text-white">© max4bd 2024</p>
     </main>
   );
 }
