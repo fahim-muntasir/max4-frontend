b5197a44d69ec31d420bacf3bf918985ef29ba53
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-10-01 00:29:42 +0600

    fix title of about page

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -6,8 +6,8 @@ export default function About() {
       <section className="py-14 lg:py-24 relative z-0 bg-gray-50">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
           <h1 className="max-w-2xl mx-auto text-center font-manrope font-bold text-4xl text-gray-900 mb-5 md:text-5xl md:leading-normal">
-            Control your Finances with our
-            <span className="text-[#FF885B]">Smart Tool </span>
+            About
+            <span className="text-[#FF885B]"> Us</span>
           </h1>
           <p className="max-w-sm mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">
             Invest intelligently and discover a better way to manage your entire
