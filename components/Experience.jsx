fa30f498da407450afc2833a95796078e691445d
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-10-17 15:55:56 +0600

    created modal for gallery

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Experience() {
   return (
-    <section className="text-gray-600 body-font my-36">
+    <section className="text-gray-600 body-font my-16 md:my-36">
       <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
--- components/gallery/Gallery.jsx
+++ components/gallery/Gallery.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 export default function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(null); // State to hold selected image
+  const [isOpen, setIsOpen] = useState(false); // State to toggle modal visibility
+
+  const images = [
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg",
+  ];
+
+  // Function to handle image click and open modal
+  const openModal = (image) => {
+    setSelectedImage(image); // Set clicked image as selected
+    setIsOpen(true); // Open modal
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsOpen(false); // Close modal
+    setSelectedImage(null); // Reset selected image
+  };
+
   return (
     <>
       <section className="py-14 lg:py-24 relative z-0 bg-gray-50 mb-10">
@@ -15,94 +47,44 @@ export default function Gallery() {
           </p>
         </div>
       </section>
+
       <section className="container mx-auto py-14 px-4 md:px-0">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-              alt=""
-            />
-          </div>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img
+                className="h-auto max-w-full rounded-lg cursor-pointer"
+                src={image}
+                alt={`Gallery Image ${index + 1}`}
+                onClick={() => openModal(image)} // Open modal on click
+              />
+            </div>
+          ))}
         </div>
       </section>
+
+      {/* Modal for larger image */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+          onClick={closeModal} // Close modal when clicking outside image
+        >
+          <div className="relative mx-5" onClick={(e) => e.stopPropagation()}>
+            {/* Stop propagation when clicking inside modal */}
+            <img
+              src={selectedImage}
+              alt="Selected Large"
+              className="max-w-full max-h-screen object-contain rounded-lg"
+            />
+            <button
+              className="absolute top-4 right-4 text-white text-2xl bg-red-400 rounded-full p-2 h-10 w-10 hover:bg-red-500"
+              onClick={closeModal} // Close modal on button click
+            >
+              <IoClose />
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
